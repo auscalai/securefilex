62f0dcb606e3077ce5d377f6cae353e291606968
Ausca <promustdie@gmail.com>
2025-11-13 03:24:18 +0800

    removed deepface react

--- faceID/deepface-react-ui/scripts/dockerize.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-
-# remove exisiting images
-# docker rm -f $(docker ps -a -q --filter "ancestor=deepface-react-app")
-
-# docker build -t deepface-react-app .
-
-docker run -p 3000:3000 deepface-react-app
